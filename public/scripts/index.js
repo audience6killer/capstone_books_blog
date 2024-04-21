814234bbc9c1d137f5c0c9b45dc8cff475cf6b88
vill4in <j.adrian.pulido.s@gmail.com>
2024-04-21 02:16:45 -0600

    Both search methods working

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,28 +1,37 @@
 
-/*$("input[name = 'bookISBN']").prop('disabled', true);
-$("input[value = 'title']").prop('checked', true);
-$("input[value = 'isbn']").prop('checked', false);
-
-$("input[value = 'title']").click(function(event){
-    console.log("Clicked");
-    $("input[value = 'isbn']").prop('checked', false);
-    $("input[name = 'bookISBN']").val('');
-    $("input[name = 'bookISBN']").prop('disabled', true);
-    $("input[name = 'bookTitle']").prop('disabled', false);
-    $("input[name = 'bookTitle']").focus();
-});
-
-$("input[value = 'isbn']").click(function(event){
-    console.log("Clicked");
-    $("input[value = 'title']").prop('checked', false);
-    $("input[name = 'bookTitle']").val('');
-    $("input[name = 'bookTitle']").prop('disabled', true);
-    $("input[name = 'bookISBN']").prop('disabled', false);
-});
-*/
 $("#datepicker").datepicker({
     format: "yyyy",
     viewMode: "years",
     minViewMode: "years",
     endDate: '+0d',
+});
+
+$(function() {
+    $("#resultsTable tbody tr").on("click", function() {
+        var rowData = $(this).find("td").map(function() {
+            return $(this).text();
+        }).get();
+        $("#resultsTable tbody tr").removeClass("table-active");
+
+        $(this).addClass('table-active');
+        $("#selectedOption").attr('value', rowData[0]);
+
+        console.log("Clicked row data:", rowData);
     });
+});
+
+$(function ($) {
+    var $inputs = $('input[name=author],input[name=title]');
+    $inputs.on('input', function () {
+        // Set the required property of the other input to false if this input is not empty.
+        $inputs.not(this).prop('required', !$(this).val().length);
+    });
+});
+
+
+
+$(".table > tr").on('click', () => {
+    console.log(this);
+    $(this).addClass('table-active');
+});
+
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import axios from "axios";
+import axios, { all } from "axios";
 import pg from "pg";
 
 
@@ -8,6 +8,54 @@ const API_URL = "https://openlibrary.org/search.json";
 const ISBN_URL = "http://openlibrary.org/api/volumes/brief/"; // <id-type>/<id-value>.json"
 const app = express();
 const port = 3000;
+
+var values = [];
+
+function parseCodeBookInfo(resData){
+    const keys = Object.keys(resData.records);
+    const items = keys.map(key => {
+        return resData.records[key];
+    });
+    var entries = items.map(item => {
+        return {
+            title: item.data.title,
+            author: item.data.authors['0'].name,
+            publishYear: item.publishDates[0],
+            isbn: item.isbns[0],
+            imgUrl: item.data.cover.medium,
+        };
+    });
+    values = entries;
+
+    return entries;
+}
+
+function parseBookInfo(resData){
+    const numFound = resData['numFound'];
+    const maxItems = numFound > 0 ? 10 : numFound;
+
+    const allItems = resData['docs'].slice(0, maxItems);
+
+    var parsedItems = allItems.filter(element => 'isbn' in element).map((element) => {
+        if(element['isbn'])
+        {
+            return {
+                title: element['title'],
+                author: element['author_name'][0],
+                publishYear: element['first_publish_year'],
+                isbn: element['isbn'].length > 1 ? element['isbn'][0] : element['isbn'],
+                imgUrl: "https://covers.openlibrary.org/b/isbn/" + (element['isbn'].length > 1 ? element['isbn'][0] : element['isbn']) + '-M.jpg',
+    
+            };
+        }
+        
+
+    });
+
+    console.log(parsedItems);
+    return parsedItems;
+
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,33 +69,45 @@ app.get('/new', (req, res) => {
     res.render('new.ejs');
 });
 
-app.post('/search-book', async (req, res) => {
-    if(req.body.searchType === "title"){
-        try {
-            const response = await axios.get(API_URL, {
-                params: {
-                    title: req.body.bookTitle,
-                }
-            });
-            console.log(response.data);
-            res.send(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    } else if(req.body.searchType === "isbn"){
-        try {
-            const response = await axios.get(ISBN_URL + 'isbn/' + req.body.bookISBN + '.json');
-            res.send(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-        
-    }
-    //console.log(JSON.stringify(req.body));
-    //const searchType = req.body.searchType;
-    //console.log("Search Type: " + searchType);
+app.post('/advanced-search', async(req, res) => {
+    console.log(req.body);
 
-    //res.send("Searching");
+    const params = {
+        title: req.body.title,
+        author: req.body.author,
+        language: req.body.language,
+        publish_year: "[* TO " + req.body.publish_year + "]",
+    }
+
+    try {
+        const response = await axios.get(API_URL, {
+            params: params,
+        });
+        // TODO: Fix publish year parser
+        var parsedResults = parseBookInfo(response.data);
+        res.render('search-results.ejs', {searchResults: parsedResults});
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.post('/code-search', async (req, res) => {
+    
+    console.log(req.body);
+    try {
+        const response = await axios.get(ISBN_URL + req.body.codeType  + '/' + req.body.bookISBN + '.json');
+        console.log(response.data);
+        var parsedResults = parseCodeBookInfo(response.data);
+        res.render('search-results.ejs', {searchResults: parsedResults});
+    } catch (error) {
+        console.error(error);
+    }
+    
+});
+
+app.post('/write-review', async (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.listen(port, ()=>{
